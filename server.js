a0a7c5cedf8e9b474b13f2ed5f6cf628ecc56ee3
Jorge Mendoza Osorio <jdevcoder000@gmail.com>
2026-02-28 21:56:28 -0600

    Servidor IoT

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+let comando = "";
+
+app.get('/Comando', (req, res) => {
+    const texto = req.query.texto || "";
+
+    if (texto.toLowerCase().includes("prende")) {
+        comando = "ON";
+    } 
+    else if (texto.toLowerCase().includes("apaga")) {
+        comando = "OFF";
+    }
+
+    res.send("Comando recibido: " + comando);
+});
+
+app.get('/LeerComando', (req, res) => {
+    res.send(comando);
+    comando = "";
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
